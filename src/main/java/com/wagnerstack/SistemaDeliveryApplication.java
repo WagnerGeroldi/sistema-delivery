2cb3a61e74b0ee6e93ebab669d7f62696445fe93
WagnerGeroldi <wagnergeroldi@gmail.com>
2022-09-02 18:08:45 -0300

    initial commit

---
+++ src/main/java/com/wagnerstack/SistemaDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.wagnerstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeliveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeliveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagnerstack/SistemaDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagnerstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
